38247717990d644e3494955ee79d93e21b78ebd7
Harsha <harsha@shipx.in>
2022-03-09 07:03:29 +0000

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,28 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!"
   end
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+
+  end
+  
+  def new
+
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
 end
